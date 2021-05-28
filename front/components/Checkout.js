716fb8f7161323be3b4784a1c91c41c529ee100b
Jaespina <jalbertoespinardez@gmail.com>
2021-05-28 03:14:55 +0200

    Orders

--- front/components/Checkout.js
+++ front/components/Checkout.js
@@ -1,65 +1,105 @@
+import { useMutation } from '@apollo/client';
 import {
-    CardElement,
-    Elements,
-    useElements,
-    useStripe,
-  } from '@stripe/react-stripe-js';
-  import { loadStripe } from '@stripe/stripe-js';
-  import nProgress from 'nprogress';
-  import { useState } from 'react';
-  import styled from 'styled-components';
-  import SickButton from './styles/SickButton';
-  
-  const CheckoutFormStyles = styled.form`
-    box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
-    border: 1px solid rgba(0, 0, 0, 0.06);
-    border-radius: 5px;
-    padding: 1rem;
-    display: grid;
-    grid-gap: 1rem;
-  `;
-  
-  const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
-  
-  function CheckoutForm() {
-    const [error, setError] = useState();
-    const [loading, setLoading] = useState();
-    const stripe = useStripe();
-    const elements = useElements();
-    async function handleSubmit(e) {
-      e.preventDefault();
-      setLoading(true);
-      nProgress.start();
-      const { error, paymentMethod } = await stripe.createPaymentMethod({
-        type: 'card',
-        card: elements.getElement(CardElement),
-      });
-  
-      console.log(paymentMethod);
-  
-      if (error) {
-        setError(error);
+  CardElement,
+  Elements,
+  useElements,
+  useStripe,
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import gql from 'graphql-tag';
+import { useRouter } from 'next/dist/client/router';
+import nProgress from 'nprogress';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useCart } from '../lib/cartState';
+import SickButton from './styles/SickButton';
+import { CURRENT_USER_QUERY } from './User';
+
+const CheckoutFormStyles = styled.form`
+  box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
+  border: 1px solid rgba(0, 0, 0, 0.06);
+  border-radius: 5px;
+  padding: 1rem;
+  display: grid;
+  grid-gap: 1rem;
+`;
+
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    checkout(token: $token) {
+      id
+      charge
+      total
+      item {
+        id
+        name
       }
-  
-      setLoading(false);
-      nProgress.done();
     }
-    return (
-      <CheckoutFormStyles onSubmit={handleSubmit}>
-        {error && <p>{error.message}</p>}
-        <CardElement />
-        <SickButton>Checkout</SickButton>
-      </CheckoutFormStyles>
-    );
   }
-  
-  function Checkout() {
-    return (
-      <Elements stripe={stripeLib}>
-        <CheckoutForm />
-      </Elements>
-    );
+`;
+
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+
+function CheckoutForm() {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+  const stripe = useStripe();
+  const elements = useElements();
+  const router = useRouter();
+  const { closeCart } = useCart();
+  const [checkout, { error: graphQLError }] = useMutation(
+    CREATE_ORDER_MUTATION,
+    {
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
+  );
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    nProgress.start();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+
+    console.log(paymentMethod);
+
+    if (error) {
+      setError(error);
+      nProgress.done();
+      return;
+    }
+    const order = await checkout({
+      variables: {
+        token: paymentMethod.id,
+      },
+    });
+
+    console.log(order);
+    router.push({
+      pathname: '/order/[id]',
+      query: { id: order.data.checkout.id },
+    });
+    closeCart();
+    setLoading(false);
+    nProgress.done();
   }
-  
-  export { Checkout };
-  
+  return (
+    <CheckoutFormStyles onSubmit={handleSubmit}>
+      {error && <p>{error.message}</p>}
+      {graphQLError && <p>{graphQLError.message}</p>}
+      <CardElement />
+      <SickButton>Checkout</SickButton>
+    </CheckoutFormStyles>
+  );
+}
+
+function Checkout() {
+  return (
+    <Elements stripe={stripeLib}>
+      <CheckoutForm />
+    </Elements>
+  );
+}
+
+export { Checkout };
--- front/lib/formatMoney.js
+++ front/lib/formatMoney.js
@@ -1,16 +1,15 @@
 export default function formatMoney(amount = 0) {
-    const options = {
-      style: 'currency',
-      currency: 'EUR',
-      minimunFractionDigits: 2,
-    };
-  
-    if (amount % 100 === 0) {
-      options.minimunFractionDigits = 0;
-    }
-  
-    const formatter = Intl.NumberFormat('es-ES', options);
-  
-    return formatter.format(amount / 100);
+  const options = {
+    style: 'currency',
+    currency: 'EUR',
+    minimunFractionDigits: 2,
+  };
+
+  if (amount % 100 === 0) {
+    options.minimunFractionDigits = 0;
   }
-  
+
+  const formatter = Intl.NumberFormat('es-ES', options);
+
+  return formatter.format(amount / 100);
+}
